93042b5ed7c5199f7791eed346248684d73984ab
Miguel Rengifo <miguelreng@gmail.com>
2026-02-18 10:49:16 -0500

    Configure Astro for Vercel deployment with SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
-import node from '@astrojs/node';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
-  output: 'static',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), react(), mdx()],
 });
